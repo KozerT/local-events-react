ec5aa456f775b2444a6a200c73cf0692f4b298e6
KozerT <kozer.vizastaff@gmail.com>
2024-03-24 18:36:20 +0100

    feat: enable delite functionality on a EventDetails component

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,12 +1,13 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
 import Header from "../Header.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock";
 
 export default function EventDetails() {
   const params = useParams();
+  const navigate = useNavigate();
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) =>
@@ -15,6 +16,22 @@ export default function EventDetails() {
         id: params.id,
       }),
   });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    if (params.id) {
+      mutate({ id: params.id });
+    }
+  }
 
   let content;
 
@@ -29,13 +46,6 @@ export default function EventDetails() {
   if (isError) {
     content = (
       <>
-        <header>
-          <h1>{data.title}</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
         <div id="event-details-content" className="center">
           <ErrorBlock
             title="Failed to fetch event"
@@ -55,15 +65,22 @@ export default function EventDetails() {
       month: "short",
       year: "numeric",
     });
-    return (
+    content = (
       <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
         <div id="event-details-content">
           <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
           <div id="event-details-info">
             <div>
               <p id="event-details-location">{data.location}</p>
               <time dateTime={`Todo-DateT$Todo-Time`}>
-                {formattedDate} @ {data.time}{" "}
+                {formattedDate} @ {data.time}
               </time>
             </div>
             <p id="event-details-description">{data.description}</p>
